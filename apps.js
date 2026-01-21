58fc731594ec949e2c53cccc3ac180a4fce52103
Hernannnnnn <megaramentar38@gmail.com>
2026-01-21 14:52:44 +0700

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,13 @@
+new Swiper('.swiper-container',{
+    direction:'vertical'
+});
+
+const header=document.getElementById('header');
+const nav=document.getElementById('nav');
+const mobileMenu=document.getElementById('mobileMenu');
+
+window.addEventListener('scroll',()=>{
+    header.classList.toggle('scrolled',window.scrollY>50)
+});
+
+mobileMenu.onclick=()=>nav.classList.toggle('active');
